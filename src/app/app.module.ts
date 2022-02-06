746ab86faf75cf342b95c907a5f2f3ae6bd53f02
Mina Dawoud <mina.dawood.s@gmail.com>
2022-02-06 21:58:43 +0400

    remove pagination pipe

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { InvoiceTemplateComponent } from './invoice-template/invoice-template.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-import { PaginationPipe } from './pipes/pagination.pipe';
 
 @NgModule({
   declarations: [
     AppComponent,
     InvoiceTemplateComponent,
-    PaginationPipe
   ],
   imports: [
     BrowserModule,
